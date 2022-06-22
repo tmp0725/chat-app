dc69a6ba43cda4a2778322a7988c90dbfa9be35f
tmp0725 <requiem202004010725@yahoo.co.jp>
2022-06-22 21:44:18 +0900

    delete unnecessary files

--- pages/socket.tsx
+++
@@ -1,6 +0,0 @@
-import { io, Socket } from "socket.io-client";
-import { DefaultEventsMap } from "socket.io/dist/typed-events";
-
-export const socket: Socket<DefaultEventsMap, DefaultEventsMap> = io(
-  "http://localhost:4000"
-);
